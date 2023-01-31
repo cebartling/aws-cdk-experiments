20617b661917d521ab43b42ece6a254a103add60
Christopher Bartling <chris.bartling@gmail.com>
2023-01-31 00:07:55 -0600

    feat: updated bucket

--- hello-cdk/lib/hello-cdk-stack.ts
+++ hello-cdk/lib/hello-cdk-stack.ts
@@ -8,7 +8,9 @@ export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
     });
   }
 }
