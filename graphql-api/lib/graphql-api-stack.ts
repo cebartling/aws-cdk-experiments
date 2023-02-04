10ee6d8c85a73144e9f475985db18358d0c6f027
Christopher Bartling <chris.bartling@gmail.com>
2023-02-03 23:05:41 -0600

    chore: checkpoint

--- graphql-api/lib/graphql-api-stack.ts
+++ graphql-api/lib/graphql-api-stack.ts
@@ -1,27 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import {CfnApiKey, CfnDataSource, CfnGraphQLApi, CfnGraphQLSchema, CfnResolver} from 'aws-cdk-lib/aws-appsync';
+import {Effect, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
+import {CfnOutput, RemovalPolicy, Stack, StackProps} from "aws-cdk-lib";
+import {AttributeType, BillingMode, Table, TableClass} from "aws-cdk-lib/aws-dynamodb";
 import {Construct} from 'constructs';
+import {readFileSync} from "fs";
 
-export class GraphqlApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class GraphqlApiStack extends Stack {
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const tableProps = {
       partitionKey: {
         name: 'Id',
-        type: dynamodb.AttributeType.STRING
+        type: AttributeType.STRING
       },
       sortKey: {
         name: 'CreatedDate',
-        type: dynamodb.AttributeType.STRING
+        type: AttributeType.STRING
       },
-      billingMode: dynamodb.BillingMode.PROVISIONED,
+      billingMode: BillingMode.PROVISIONED,
       readCapacity: 2,
       writeCapacity: 2,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      tableClass: dynamodb.TableClass.STANDARD
+      removalPolicy: RemovalPolicy.DESTROY,
+      tableClass: TableClass.STANDARD
     };
-    const table = new dynamodb.Table(
+    const table = new Table(
       this,
       'Foobars',
       tableProps
@@ -36,8 +42,80 @@ export class GraphqlApiStack extends cdk.Stack {
         targetUtilizationPercent: 75
       });
 
-    new cdk.CfnOutput(this, 'FoobarsTableName', {
+    new CfnOutput(this, 'FoobarsTableName', {
       value: table.tableName
     });
+
+    // Add AppSync GraphQL API, generate an API key for use and a schema
+    const api = new CfnGraphQLApi(this, "graphql-api-id", {
+      name: "graphql-api-name",
+      authenticationType: "API_KEY",
+      xrayEnabled: true
+    });
+
+    const apiKey = new CfnApiKey(this, 'graphql-api-key', {
+      apiId: api.attrApiId,
+    });
+
+    const schema = new CfnGraphQLSchema(this, "graphql-api-schema", {
+      apiId: api.attrApiId,
+      definition: readFileSync("./graphql/schema.graphql").toString(),
+    });
+
+    // Add lambda, plus the required datasource and resolvers, as well as create an invoke lambda role for AppSync
+    const invokeLambdaRole = new Role(this, "AppSync-InvokeLambdaRole", {
+      assumedBy: new ServicePrincipal("appsync.amazonaws.com"),
+    });
+
+    const messagesLambdaFunction = new NodejsFunction(this, "messages-lambda-id", {
+      entry: "./src/lambda/index.ts",
+      handler: "handler",
+      functionName: "lambda-function-name",
+      runtime: Runtime.NODEJS_14_X,
+    });
+
+    const messagesDataSource = new CfnDataSource(this, "messages-datasource", {
+      apiId: api.attrApiId,
+      // Note: property 'name' cannot include hyphens
+      name: "MessagesDataSource",
+      type: "AWS_LAMBDA",
+      lambdaConfig: {
+        lambdaFunctionArn: messagesLambdaFunction.functionArn
+      },
+      serviceRoleArn: invokeLambdaRole.roleArn
+    });
+
+    const messagesResolver = new CfnResolver(this, "messages-resolver", {
+      apiId: api.attrApiId,
+      typeName: "Query",
+      fieldName: "messages",
+      dataSourceName: messagesDataSource.name,
+    });
+
+    const welcomeMessageResolver = new CfnResolver(this, "welcomeMessage-resolver", {
+      apiId: api.attrApiId,
+      typeName: "MessageQuery",
+      fieldName: "welcomeMessage",
+      dataSourceName: messagesDataSource.name,
+    });
+
+    const farewellMessageResolver = new CfnResolver(this, "farewellMessage-resolver", {
+      apiId: api.attrApiId,
+      typeName: "MessageQuery",
+      fieldName: "farewellMessage",
+      dataSourceName: messagesDataSource.name,
+    });
+
+    // Ensures that the resolvers are created after the schema.
+    messagesResolver.addDependsOn(schema);
+    welcomeMessageResolver.addDependsOn(schema);
+    farewellMessageResolver.addDependsOn(schema);
+
+    // Ensures that AppSync is able to invoke the lambda function.
+    invokeLambdaRole.addToPolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
+      resources: [messagesLambdaFunction.functionArn],
+      actions: ["lambda:InvokeFunction"]
+    }));
   }
 }
