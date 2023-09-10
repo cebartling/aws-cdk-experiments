0c6153a71e7b50d41827130656adc0e9ecd8c0ae
Christopher Bartling <chris.bartling@gmail.com>
2023-09-09 21:06:53 -0500

    feat: created Cognito user pool

--- cognito-user-pool/lib/cognito-user-pool-stack.ts
+++ cognito-user-pool/lib/cognito-user-pool-stack.ts
@@ -1,16 +1,67 @@
 import * as cdk from 'aws-cdk-lib';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CognitoUserPoolStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const userpool = new cognito.UserPool(this, 'experiment-user-pool', {
+      userPoolName: 'experiment-user-pool',
+      signInAliases: {
+        email: true,
+      },
+      selfSignUpEnabled: true,
+      autoVerify: {
+        email: true,
+      },
+      userVerification: {
+        emailSubject: 'You need to verify your email',
+        emailBody: 'Thanks for signing up! Your verification code is {####}.', // # This placeholder is a must if code is selected as preferred verification method
+        emailStyle: cognito.VerificationEmailStyle.CODE,
+      },
+      standardAttributes: {
+        familyName: {
+          mutable: false,
+          required: true,
+        },
+        address: {
+          mutable: true,
+          required: false,
+        },
+      },
+      customAttributes: {
+        tenantId: new cognito.StringAttribute({
+          mutable: false,
+          minLen: 10,
+          maxLen: 30,
+        }),
+        createdAt: new cognito.DateTimeAttribute(),
+        employeeId: new cognito.NumberAttribute({
+          mutable: false,
+          min: 1,
+          max: 100,
+        }),
+        isAdmin: new cognito.BooleanAttribute({
+          mutable: false,
+        }),
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: false,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CognitoUserPoolQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const appClient = userpool.addClient('experiment-app-client', {
+      userPoolClientName: 'experiment-app-client',
+      authFlows: {
+        userPassword: true,
+      },
+    });
   }
 }
