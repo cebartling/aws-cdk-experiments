90c3911650fe1b9d06febe1730522f08366ceac5
Christopher Bartling <chris.bartling@gmail.com>
2023-09-10 16:14:45 -0500

    feat: configuring Amplify

---
+++ remix-app/app/Amplify.server.ts
@@ -0,0 +1,57 @@
+import { Amplify, Auth } from 'aws-amplify';
+import * as process from "process";
+
+Amplify.configure({
+  Auth: {
+    region: process.env.AWS_REGION,
+    userPoolId: process.env.AWS_USER_POOL_ID,
+    userPoolWebClientId: process.env.AWS_USER_POOL_CLIENT_ID,
+    // OPTIONAL - This is used when autoSignIn is enabled for Auth.signUp
+    // 'code' is used for Auth.confirmSignUp, 'link' is used for email link verification
+    signUpVerificationMethod: 'code', // 'code' | 'link'
+
+    // OPTIONAL - Configuration for cookie storage
+    // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+    cookieStorage: {
+      // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+      domain: '.pintailconsultingllc.com',
+      // OPTIONAL - Cookie path
+      path: '/',
+      // OPTIONAL - Cookie expiration in days
+      expires: 365,
+      // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+      sameSite: 'lax',
+      // OPTIONAL - Cookie secure flag
+      // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+      secure: true,
+    },
+
+    // OPTIONAL - customized storage object
+    // storage: MyStorage,
+
+    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+    authenticationFlowType: 'USER_PASSWORD_AUTH',
+
+    // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
+    // clientMetadata: {myCustomKey: 'myCustomValue'},
+
+    // OPTIONAL - Hosted UI configuration
+    // oauth: {
+    //   domain: 'your_cognito_domain',
+    //   scope: [
+    //     'phone',
+    //     'email',
+    //     'profile',
+    //     'openid',
+    //     'aws.cognito.signin.user.admin',
+    //   ],
+    //   redirectSignIn: 'http://localhost:3000/',
+    //   redirectSignOut: 'http://localhost:3000/',
+    //   responseType: 'code', // or 'token', note that REFRESH token will only be generated when the responseType is code
+    // },
+  },
+});
+
+
+// You can get the current config object
+export const amplifyAuthOptions = Auth.configure();
