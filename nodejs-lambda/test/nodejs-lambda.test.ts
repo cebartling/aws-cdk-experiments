99b2664bd22988443464fe1d6619c11e75261391
Christopher Bartling <chris.bartling@gmail.com>
2023-09-25 20:59:33 -0500

    feat: creating new CDK project

---
+++ nodejs-lambda/test/nodejs-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NodejsLambda from '../lib/nodejs-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nodejs-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NodejsLambda.NodejsLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
